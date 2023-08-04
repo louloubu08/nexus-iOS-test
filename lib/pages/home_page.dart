c44d8fff0ca4e97c4bbe06a267d2ea3d5d9dfc21
Tetstt <120795261+YursenIndustries@users.noreply.github.com>
2023-08-04 10:16:35 +0200

    Mainly improved the Login and Register pages

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -11,12 +12,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  // user
+  final currentUser
+
+
+  //sign user out
+  void signOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     Firebase.initializeApp();
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       appBar: AppBar(
         title: Text("NEXUS"),
+        backgroundColor: Colors.grey[900],
+        elevation: 0,
+        actions: [
+          // sign out button
+          IconButton(
+            onPressed: signOut,
+            icon: Icon(Icons.logout),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          // the wall
+
+          // post message
+
+          // loged in as
+        ],
       ),
     );
   }
